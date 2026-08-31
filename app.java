31475f0b265efe7c50698f7c86cbac2610b49b19
Rohith-9010 <gonerohith8@gmail.com>
2026-08-31 07:43:37 +0530

    updated new java code

--- app.java
+++ app.java
@@ -4,3 +4,17 @@ public class HelloWorld {
     }
 }
 
+//dev 2 code
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 7; // Change this number to test
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
+

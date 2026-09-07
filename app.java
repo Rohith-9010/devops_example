215a76ef7f0b5dadebdbe090247c83c1c119ea5b
Rohith-9010 <gonerohith8@gmail.com>
2026-09-07 11:21:46 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         // this is comment 
+        //comments are helpful
          System.out.println("Jenkins                   class !");
     }
 }
